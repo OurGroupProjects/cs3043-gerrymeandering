8fb7c31b44b6cb5dfa3c929770e54d398eef62c4
Isaac Woods <isaac.woods.official@gmail.com>
2019-04-27 16:31:50 -0400

    Implement parallel downloading of district data

--- data/download-data.py
+++ data/download-data.py
@@ -1,17 +1,22 @@
 from urllib import request
 from zipfile import ZipFile
+from multiprocessing import Pool
 
 base_url = 'http://cdmaps.polisci.ucla.edu/shp/'
 base_file_name = 'districts'
 file_extension = '.zip'
 num_padding = 3
 last_district = 114
+num_processes = 4
 
-def getDistrictInfo(num):
+def getDistrictInfo(i):
+    num = i+1
     file_name = base_file_name + str(num).rjust(num_padding, '0') + file_extension
     local_file_name, headers = request.urlretrieve(base_url+file_name)
     ZipFile(local_file_name, 'r').extractall()
     request.urlcleanup()
+    return
 
-for i in range(last_district):
-    getDistrictInfo(i+1)
+if __name__ == "__main__":
+    with Pool(processes=num_processes) as pool:
+        [i for i in pool.imap_unordered(getDistrictInfo, range(last_district))]
