780f190040ea34bd5d156deb9debcbc574f6c296
Isaac Woods <isaac.woods.official@gmail.com>
2019-04-27 16:15:13 -0400

    Download all district data, slight refactor

--- data/download-data.py
+++ data/download-data.py
@@ -7,13 +7,11 @@ file_extension = '.zip'
 num_padding = 3
 last_district = 114
 
-for i in range(last_district):
-    num = i+1
+def getDistrictInfo(num):
     file_name = base_file_name + str(num).rjust(num_padding, '0') + file_extension
     local_file_name, headers = request.urlretrieve(base_url+file_name)
-    with ZipFile(local_file_name, 'r') as zipfile:
-        zipped_files = zipfile.namelist()
-        for f in zipped_files:
-            if f.endswith('.shp'):
-                zipfile.extract(f, 'shp')
+    ZipFile(local_file_name, 'r').extractall()
     request.urlcleanup()
+
+for i in range(last_district):
+    getDistrictInfo(i+1)
